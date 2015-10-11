2a1c1a1f6d21894bf3cb9cc2397d1ae9a5d90ab1
freek <freek@spatie.be>
2015-10-11 19:30:04 +0200

    psr2

--- tests/BladeTest.php
+++ tests/BladeTest.php
@@ -7,10 +7,26 @@ class BladeTest extends TestCase
     /**
      * @test
      */
-    public function all_blade_directives_will_work_even_if_there_is_nobody_logged_in()
+    public function all_blade_directives_will_evaluate_falsly_when_there_is_nobody_logged_in()
     {
         $role = 'admin';
         $roles = [$role];
+
+        $this->assertEquals('does not have role', $this->renderView('role', [$role]));
+        $this->assertEquals('does not have role', $this->renderView('hasrole', [$role]));
+        $this->assertEquals('does not have all of the given roles', $this->renderView('hasAllRoles', $roles));
+        $this->assertEquals('does not have any of the given roles', $this->renderView('hasAnyRole', $roles));
+    }
+
+    /**
+     * @test
+     */
+    public function all_blade_directives_will_evaluate_falsly_when_there_is_somebody_with_no_roles_or_permissions_is_logged_in()
+    {
+        $role = 'admin';
+        $roles = [$role];
+
+        $this->actingAs(User::create(['email' => 'someboy@guest.com']);
 
         $this->assertEquals('does not have role', $this->renderView('role', [$role]));
         $this->assertEquals('does not have role', $this->renderView('hasrole', [$role]));
