02ebe59add50de4efa7c8772ff17eb97d51be9bb
Freek Van der Herten <freek@spatie.be>
2015-12-22 00:27:57 +0100

    Update PermissionServiceProvider.php

--- src/PermissionServiceProvider.php
+++ src/PermissionServiceProvider.php
@@ -24,7 +24,7 @@ class PermissionServiceProvider extends ServiceProvider
             // Publish the migration
             $timestamp = date('Y_m_d_His', time());
             $this->publishes([
-                __DIR__.'/../resources/migrations/create_permission_tables.php.stub' => $this->app->basePath().'/'.'database/migrations/'.$timestamp.'_create_permission_tables.php',
+                __DIR__.'/../resources/migrations/create_permission_tables.php.stub' => $this->app->databasePath().'/migrations/'.$timestamp.'_create_permission_tables.php',
             ], 'migrations');
         }
 
