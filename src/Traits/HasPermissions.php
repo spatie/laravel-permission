b8e64ebbc56c79a068385e7678050e1f50985cc5
Peter Quentin <peter@love4work.com>
2016-07-21 10:32:11 +0200

    Remove joinings option from givePermissionTo function and cleanup code

--- src/Traits/HasPermissions.php
+++ src/Traits/HasPermissions.php
@@ -16,14 +16,12 @@ trait HasPermissions
      */
     public function givePermissionTo($permissions, ...$params)
     {
-        $joinings = $this->resolveJoiningAttributes($params);
-
         if (is_string($permissions) && $this->usingMultipleArguments($params)) {
             array_unshift($params, $permissions);
             $permissions = $params;
         }
 
-        $this->savePermissions($permissions, $joinings);
+        $this->savePermissions($permissions);
 
         $this->forgetCachedPermissions();
 
@@ -34,16 +32,15 @@ trait HasPermissions
      * Save the given permissions to a role.
      *
      * @param array|Permission|\Illuminate\Support\Collection $permissions
-     * @param array $joinings
      * 
      * @return array|\Illuminate\Database\Eloquent\Model
      */
-    protected function savePermissions($permissions, array $joinings = [])
+    protected function savePermissions($permissions)
     {
         $permissions = $this->getStoredPermission($permissions);
 
         if($permissions instanceof \Illuminate\Database\Eloquent\Collection) {
-            return $this->permissions()->saveMany($permissions, $joinings);
+            return $this->permissions()->saveMany($permissions);
         }
 
         return $this->permissions()->save($permissions);
@@ -92,16 +89,4 @@ trait HasPermissions
         return count($params) > 0;
     }
 
-    /**
-     * @param $params
-     * @return array
-     */
-    protected function resolveJoiningAttributes(&$params)
-    {
-        if(is_array(end($params)) && !empty($params)) {
-            return array_pop($params);
-        }
-
-        return [];
-    }
 }
