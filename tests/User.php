554e43d5576d1c4c723d19774e7e6f34c7c41e9b
Freek Van der Herten <freek@spatie.be>
2016-08-26 09:17:52 +0200

    Applied fixes from StyleCI (#113)

--- tests/User.php
+++ tests/User.php
@@ -3,11 +3,11 @@
 namespace Spatie\Permission\Test;
 
 use Illuminate\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\Access\Authorizable;
 use Spatie\Permission\Traits\HasRoles;
-use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 
 class User extends Model implements AuthorizableContract, AuthenticatableContract
 {
