ae3daa8f6dc64ab7edda436452c998e46f5ae32f
Freek Van der Herten <freek@spatie.be>
2017-02-23 23:01:53 +0000

    Apply fixes from StyleCI

--- resources/config/laravel-permission.php
+++ resources/config/laravel-permission.php
@@ -4,7 +4,7 @@ return [
 
     'models' => [
 
-        /**
+        /*
          * When using the "HasRoles" trait from this package, we need to know which
          * Eloquent model should be used to retrieve your permissions. Of course, it
          * is often just the "Permission" model but you may use whatever you like.
@@ -15,7 +15,7 @@ return [
 
         'permission' => Spatie\Permission\Models\Permission::class,
 
-        /**
+        /*
          * When using the "HasRoles" trait from this package, we need to know which
          * Eloquent model should be used to retrieve your roles. Of course, it
          * is often just the "Role" model but you may use whatever you like.
@@ -30,14 +30,14 @@ return [
 
     'table_names' => [
 
-        /**
+        /*
          * The table that your application uses for users. This table's model will
          * be using the "HasRoles" and "HasPermissions" traits.
          */
 
         'users' => 'users',
 
-        /**
+        /*
          * When using the "HasRoles" trait from this package, we need to know which
          * table should be used to retrieve your roles. We have chosen a basic
          * default value but you may easily change it to any table you like.
@@ -45,7 +45,7 @@ return [
 
         'roles' => 'roles',
 
-        /**
+        /*
          * When using the "HasRoles" trait from this package, we need to know which
          * table should be used to retrieve your permissions. We have chosen a basic
          * default value but you may easily change it to any table you like.
@@ -53,7 +53,7 @@ return [
 
         'permissions' => 'permissions',
 
-        /**
+        /*
          * When using the "HasRoles" trait from this package, we need to know which
          * table should be used to retrieve your users permissions. We have chosen a
          * basic default value but you may easily change it to any table you like.
@@ -61,7 +61,7 @@ return [
 
         'user_has_permissions' => 'user_has_permissions',
 
-        /**
+        /*
          * When using the "HasRoles" trait from this package, we need to know which
          * table should be used to retrieve your users roles. We have chosen a
          * basic default value but you may easily change it to any table you like.
@@ -69,7 +69,7 @@ return [
 
         'user_has_roles' => 'user_has_roles',
 
-        /**
+        /*
          * When using the "HasRoles" trait from this package, we need to know which
          * table should be used to retrieve your roles permissions. We have chosen a
          * basic default value but you may easily change it to any table you like.
@@ -79,7 +79,7 @@ return [
 
     ],
 
-    /**
+    /*
      *
      * By default we'll make an entry in the application log when the permissions
      * could not be loaded. Normally this only occurs while installing the packages.
