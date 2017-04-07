7ae23be15f7479f9d51deb97b78ab29d1a5f0061
Alex Vanderbist <alex.vanderbist@gmail.com>
2017-04-07 11:59:01 +0200

    Updated tests for cache

--- tests/CacheTest.php
+++ tests/CacheTest.php
@@ -23,75 +23,73 @@ class CacheTest extends TestCase
 
         $this->assertCount(0, DB::getQueryLog());
 
-        //cache was empty, some queries should have been performed
         $this->registrar->registerPermissions();
 
         $this->assertCount(2, DB::getQueryLog());
+
+        DB::flushQueryLog();
     }
 
     /** @test */
     public function it_can_cache_the_permissions()
     {
-        //permission should be cached an no queries should be performed
         $this->registrar->registerPermissions();
-        $this->assertCount(2, DB::getQueryLog());
+
+        $this->assertCount(0, DB::getQueryLog());
     }
 
     /** @test */
     public function permission_creation_and_updating_should_flush_the_cache()
     {
         $permission = app(Permission::class)->create(['name' => 'new']);
-        $this->assertCount(3, DB::getQueryLog());
+        $this->assertCount(1, DB::getQueryLog());
 
         $this->registrar->registerPermissions();
-        $this->assertCount(5, DB::getQueryLog());
+        $this->assertCount(3, DB::getQueryLog());
 
         $permission->name = 'other name';
         $permission->save();
-        $this->assertCount(6, DB::getQueryLog());
+        $this->assertCount(4, DB::getQueryLog());
 
         $this->registrar->registerPermissions();
-        $this->assertCount(8, DB::getQueryLog());
+        $this->assertCount(6, DB::getQueryLog());
     }
 
     /** @test */
     public function role_creation_and_updating_should_flush_the_cache()
     {
         $role = app(Role::class)->create(['name' => 'new']);
-        $this->assertCount(3, DB::getQueryLog());
+        $this->assertCount(1, DB::getQueryLog());
 
         $this->registrar->registerPermissions();
-        $this->assertCount(5, DB::getQueryLog());
+        $this->assertCount(3, DB::getQueryLog());
 
         $role->name = 'other name';
         $role->save();
-        $this->assertCount(6, DB::getQueryLog());
+        $this->assertCount(4, DB::getQueryLog());
 
         $this->registrar->registerPermissions();
-        $this->assertCount(8, DB::getQueryLog());
+        $this->assertCount(6, DB::getQueryLog());
     }
 
     /** @test */
     public function user_creation_should_not_flush_the_cache()
     {
         User::create(['email' => 'new']);
-        $this->assertCount(3, DB::getQueryLog());
+        $this->assertCount(1, DB::getQueryLog());
 
         $this->registrar->registerPermissions();
-        $this->assertCount(5, DB::getQueryLog());
-
-        $this->registrar->registerPermissions();
-        $this->assertCount(5, DB::getQueryLog());
+        $this->assertCount(1, DB::getQueryLog());
     }
 
     /** @test */
     public function adding_a_permission_to_a_role_should_flush_the_cache()
     {
         $this->testUserRole->givePermissionTo($this->testUserPermission);
-        $this->assertCount(3, DB::getQueryLog());
+        $this->assertCount(1, DB::getQueryLog());
 
         $this->registrar->registerPermissions();
-        $this->assertCount(5, DB::getQueryLog());
+        $this->assertCount(3, DB::getQueryLog());
     }
 
     /** @test */
@@ -99,15 +97,15 @@ class CacheTest extends TestCase
     {
         $this->testUserRole->givePermissionTo(['edit-articles', 'edit-news']);
         $this->testUser->assignRole('testRole');
-        $this->assertCount(6, DB::getQueryLog());
+        $this->assertCount(4, DB::getQueryLog());
 
         $this->assertTrue($this->testUser->hasPermissionTo('edit-articles'));
-        $this->assertCount(10, DB::getQueryLog());
+        $this->assertCount(8, DB::getQueryLog());
 
         $this->assertTrue($this->testUser->hasPermissionTo('edit-news'));
-        $this->assertCount(10, DB::getQueryLog());
+        $this->assertCount(8, DB::getQueryLog());
 
         $this->assertTrue($this->testUser->hasPermissionTo('edit-articles'));
-        $this->assertCount(10, DB::getQueryLog());
+        $this->assertCount(8, DB::getQueryLog());
     }
 }
