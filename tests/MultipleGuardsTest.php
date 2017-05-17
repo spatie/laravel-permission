d37de7a2d4a3b15cba21f109e5c46475da396481
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2017-05-17 16:48:27 +0200

    Add failing test

---
+++ tests/MultipleGuardsTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Spatie\Permission\Test;
+
+use Spatie\Permission\Models\Permission;
+
+class MultipleGuardsTest extends TestCase
+{
+    /** @test */
+    public function it_can_give_a_permission_to_a_model_that_uses_multiple_guards()
+    {
+        $this->testUser->givePermissionTo(Permission::create([
+            'name' => 'cannnn_dooooo',
+            'guard_name' => 'web',
+        ]));
+
+        $this->testUser->givePermissionTo(Permission::create([
+            'name' => 'cannnn_dooooo',
+            'guard_name' => 'api',
+        ]));
+
+    }
+
+    protected function getEnvironmentSetUp($app)
+    {
+        parent::getEnvironmentSetUp($app);
+
+        $app['config']->set('auth.guards', [
+            'web' => ['driver' => 'session', 'provider' => 'users'],
+            'api' => ['driver' => 'jwt', 'provider' => 'users'],
+        ]);
+    }
+}
