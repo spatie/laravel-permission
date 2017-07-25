5a84ced81ece04bda1327c70caa3f11d133339ce
freek <freek@spatie.be>
2017-07-25 15:07:17 -0400

    add extra tests

--- tests/CommandTest.php
+++ tests/CommandTest.php
@@ -17,10 +17,36 @@ class CommandTest extends TestCase
     }
 
     /** @test */
+    public function it_can_create_a_role_with_a_specific_guard()
+    {
+        Artisan::call('permission:create-role', [
+            'name' => 'new-role',
+            'guard' => 'api',
+        ]);
+
+        $this->assertCount(1, Role::where('name', 'new-role')
+            ->where('guard_name', 'api')
+            ->get());
+    }
+
+    /** @test */
     public function it_can_create_a_permission()
     {
         Artisan::call('permission:create-permission', ['name' => 'new-permission']);
 
         $this->assertCount(1, Permission::where('name', 'new-permission')->get());
+    }
+
+    /** @test */
+    public function it_can_create_a_permission_with_a_specific_guard()
+    {
+        Artisan::call('permission:create-permission', [
+            'name' => 'new-permission',
+            'guard' => 'api',
+        ]);
+
+        $this->assertCount(1, Permission::where('name', 'new-permission')
+            ->where('guard_name', 'api')
+            ->get());
     }
 }
