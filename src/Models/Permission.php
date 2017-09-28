dfb35093713ca6bcb92eced3a29ed2a6367d8e3e
Mostafa Abd El-Salam Maklad <dev.mostafa.maklad@gmail.com>
2017-09-28 17:00:23 +0300

    add laravel 5.3 support (#478)
    * add laravel 5.3 support
    
    * fix styling

--- src/Models/Permission.php
+++ src/Models/Permission.php
@@ -35,6 +35,10 @@ class Permission extends Model implements PermissionContract
             throw PermissionAlreadyExists::create($attributes['name'], $attributes['guard_name']);
         }
 
+        if (app()::VERSION < '5.4') {
+            return parent::create($attributes);
+        }
+
         return static::query()->create($attributes);
     }
 
