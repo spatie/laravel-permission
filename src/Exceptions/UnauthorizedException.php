208ac1c8f855d0f75fb7b50c634be8f1457cfb4c
Fabian Bettag <admin@meetfurs.de>
2018-01-22 23:57:12 +0100

    Store reason for failed role/permission check

--- src/Exceptions/UnauthorizedException.php
+++ src/Exceptions/UnauthorizedException.php
@@ -6,18 +6,52 @@ use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class UnauthorizedException extends HttpException
 {
+    private $requiredRoles = [];
+
+    private $requiredPermissions = [];
+
     public static function forRoles(array $roles): self
     {
-        return new static(403, 'User does not have the right roles.', null, []);
+        $message = 'User does not have the right roles.';
+
+        if (config('permission.display_permission_in_exception')) {
+            $permStr = implode(', ', $roles);
+            $message = 'User does not have the right roles. Necessary roles are '.$permStr;
+        }
+
+        $exception = new static(403, $message, null, []);
+        $exception->requiredRoles = $roles;
+
+        return $exception;
     }
 
     public static function forPermissions(array $permissions): self
     {
-        return new static(403, 'User does not have the right permissions.', null, []);
+        $message = 'User does not have the right permissions.';
+
+        if (config('permission.display_permission_in_exception')) {
+            $permStr = implode(', ', $permissions);
+            $message = 'User does not have the right permissions. Necessary permissions are '.$permStr;
+        }
+
+        $exception = new static(403, $message, null, []);
+        $exception->requiredPermissions = $permissions;
+
+        return $exception;
     }
 
     public static function notLoggedIn(): self
     {
         return new static(403, 'User is not logged in.', null, []);
+    }
+
+    public function getRequiredRoles(): array
+    {
+        return $this->requiredRoles;
+    }
+
+    public function getRequiredPermissions(): array
+    {
+        return $this->requiredPermissions;
     }
 }
