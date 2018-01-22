d2aa84ec23d8b08e13c19bd2dcc4549aad12fbd8
Ben Yanke <byanke@pcnametag.com>
2018-01-22 11:01:49 -0600

    Adding PR to create option to display permissions in exception

--- src/Exceptions/UnauthorizedException.php
+++ src/Exceptions/UnauthorizedException.php
@@ -8,12 +8,24 @@ class UnauthorizedException extends HttpException
 {
     public static function forRoles(array $roles): self
     {
+
+        if(config('permission.display_permission_in_exception')) {
+            $permStr = implode(", ", $roles);
+            return new static(403, 'User does not have the right roles. Necessary roles are ' . $permStr, null, []);
+        }
+
         return new static(403, 'User does not have the right roles.', null, []);
     }
 
     public static function forPermissions(array $permissions): self
     {
-        return new static(403, 'User does not have the right permissions.', null, []);
+
+        if(true || config('permission.display_permission_in_exception')) {
+            $permStr = implode(", ", $permissions);
+            return new static(403, 'User does not have the right permissions. Necessary permissions are ' . $permStr, null, []);
+        }
+
+       return new static(403, 'User does not have the right permissions.', null, []);
     }
 
     public static function notLoggedIn(): self
@@ -21,3 +33,4 @@ class UnauthorizedException extends HttpException
         return new static(403, 'User is not logged in.', null, []);
     }
 }
+
