6e457ec73eec3d58a2616274131fa2eca4255a5a
Ben Yanke <byanke@pcnametag.com>
2018-01-23 07:07:15 -0600

    Final (?) StyleCI fix

--- src/Exceptions/UnauthorizedException.php
+++ src/Exceptions/UnauthorizedException.php
@@ -12,6 +12,7 @@ class UnauthorizedException extends HttpException
         if(config('permission.display_permission_in_exception')) {
             $permStr = ' Necessary roles are '.implode(', ', $roles).'.';
         }
+
         return new static(403, 'User does not have the right roles.'.$permStr, null, []);
     }
 
@@ -21,6 +22,7 @@ class UnauthorizedException extends HttpException
         if(config('permission.display_permission_in_exception')) {
             $permStr = ' Necessary permissions are '.implode(', ', $permissions).'.';
         }
+
         return new static(403, 'User does not have the right permissions.', null, []);
     }
 
