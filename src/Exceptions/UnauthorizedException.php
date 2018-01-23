91a4430bd3d594b2b0a80b789dbab3caea172cda
Ben Yanke <byanke@pcnametag.com>
2018-01-23 06:55:44 -0600

    Fix style

--- src/Exceptions/UnauthorizedException.php
+++ src/Exceptions/UnauthorizedException.php
@@ -8,24 +8,20 @@ class UnauthorizedException extends HttpException
 {
     public static function forRoles(array $roles): self
     {
-
         if(config('permission.display_permission_in_exception')) {
-            $permStr = implode(", ", $roles);
-            return new static(403, 'User does not have the right roles. Necessary roles are ' . $permStr, null, []);
+            $permStr = implode(', ', $roles);
+            return new static(403, 'User does not have the right roles. Necessary roles are '.$permStr, null, []);
         }
-
         return new static(403, 'User does not have the right roles.', null, []);
     }
 
     public static function forPermissions(array $permissions): self
     {
-
-        if(true || config('permission.display_permission_in_exception')) {
-            $permStr = implode(", ", $permissions);
-            return new static(403, 'User does not have the right permissions. Necessary permissions are ' . $permStr, null, []);
+        if(config('permission.display_permission_in_exception')) {
+            $permStr = implode(', ', $permissions);
+            return new static(403, 'User does not have the right permissions. Necessary permissions are '.$permStr, null, []);
         }
-
-       return new static(403, 'User does not have the right permissions.', null, []);
+        return new static(403, 'User does not have the right permissions.', null, []);
     }
 
     public static function notLoggedIn(): self
