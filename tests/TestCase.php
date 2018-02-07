9ca14b0067f80e023529f474ac33b554d1a8e696
Chris Brown <drbyte@zen-cart.com>
2018-02-07 01:53:54 -0500

    v1-remove unused test dependencies

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,6 @@
 
 namespace Spatie\Permission\Test;
 
-use Monolog\Handler\TestHandler;
 use Spatie\Permission\Contracts\Role;
 use Illuminate\Database\Schema\Blueprint;
 use Spatie\Permission\PermissionRegistrar;
@@ -32,8 +31,6 @@ abstract class TestCase extends Orchestra
         $this->testUser = User::first();
         $this->testRole = app(Role::class)->first();
         $this->testPermission = app(Permission::class)->find(1);
-
-        $this->clearLogTestHandler();
     }
 
     /**
@@ -63,8 +60,6 @@ abstract class TestCase extends Orchestra
         ]);
 
         $app['config']->set('view.paths', [__DIR__.'/resources/views']);
-
-        $app['log']->getMonolog()->pushHandler(new TestHandler());
     }
 
     /**
@@ -106,36 +101,4 @@ abstract class TestCase extends Orchestra
         $this->testUser = $this->testUser->fresh();
     }
 
-    protected function clearLogTestHandler()
-    {
-        collect($this->app['log']->getMonolog()->getHandlers())->filter(function ($handler) {
-            return $handler instanceof TestHandler;
-        })->first(function (TestHandler $handler) {
-            $handler->clear();
-        });
-    }
-
-    protected function assertNotLogged($message, $level)
-    {
-        $this->assertFalse($this->hasLog($message, $level), "Found `{$message}` in the logs.");
-    }
-
-    protected function assertLogged($message, $level)
-    {
-        $this->assertTrue($this->hasLog($message, $level), "Couldn't find `{$message}` in the logs.");
-    }
-
-    /**
-     * @param $message
-     * @param $level
-     *
-     * @return bool
-     */
-    protected function hasLog($message, $level)
-    {
-        return collect($this->app['log']->getMonolog()->getHandlers())->filter(function ($handler) use ($message, $level) {
-            return $handler instanceof TestHandler
-                && $handler->hasRecordThatContains($message, $level);
-        })->count() > 0;
-    }
 }
