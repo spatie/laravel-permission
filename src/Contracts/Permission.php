d75b54c3e1f2d50dbfde19d07ad1d59fea2ffd56
Chris Brown <drbyte@zen-cart.com>
2018-02-23 10:45:11 -0500

    Style

--- src/Contracts/Permission.php
+++ src/Contracts/Permission.php
@@ -24,7 +24,7 @@ interface Permission
      * @return Permission
      */
     public static function findByName(string $name, $guardName): self;
-  
+
     /**
      * Find a permission by its id.
      *
