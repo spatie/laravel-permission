bc1ee9245a22a6f1aaf94fc6357fd58600cb371b
klin <konstantin.linnik@gmail.com>
2018-05-11 18:40:47 +0300

    Fix cache forget. Now it will flush only tagged cache

--- src/PermissionRegistrar.php
+++ src/PermissionRegistrar.php
@@ -42,12 +42,12 @@ class PermissionRegistrar
 
     public function forgetCachedPermissions()
     {
-        $this->cache->flush();
+        $this->cache->tags('spatie')->flush();
     }
 
     public function getPermissions($params = null): Collection
     {
-        return $this->cache->remember($this->cacheKey.($params ? '.'.implode('.', array_values($params)) : ''), config('permission.cache_expiration_time'), function () use ($params) {
+        return $this->cache->tags('spatie')->remember($this->cacheKey.($params ? '.'.implode('.', array_values($params)) : ''), config('permission.cache_expiration_time'), function () use ($params) {
             if ($params) {
                 return app(Permission::class)->where($params)->with('roles')->get();
             } else {
