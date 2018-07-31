96a82d0dfe1f2b76856ea6652dc7c12ea76db86c
zgldh <zgldh@hotmail.com>
2018-07-31 19:07:46 +0800

    Fixed HasRoles::hasRole() function
    is_array does not work to Illuminate\Support\Collection
    It needs a proper type detection.

--- src/Traits/HasRoles.php
+++ src/Traits/HasRoles.php
@@ -142,7 +142,7 @@ trait HasRoles
             return $this->roles->contains('id', $roles->id);
         }
 
-        if (is_array($roles)) {
+        if (is_array($roles) || is_a($a, Illuminate\Support\Collection::class)) {
             foreach ($roles as $role) {
                 if ($this->hasRole($role)) {
                     return true;
