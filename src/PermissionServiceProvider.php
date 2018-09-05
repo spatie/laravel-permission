fc32c658cd91431b12d4037dc6f972216a700bb8
Chris Brown <drbyte@zen-cart.com>
2018-09-05 18:04:15 -0400

    Remove extra bracket

--- src/PermissionServiceProvider.php
+++ src/PermissionServiceProvider.php
@@ -26,7 +26,7 @@ class PermissionServiceProvider extends ServiceProvider
         }
 
         if ($this->app->runningInConsole()) {
-            $this->commands([[
+            $this->commands([
                 Commands\AssignRole::class,
                 Commands\CreateRole::class,
                 Commands\CreatePermission::class,
