f86ec8b1d51770e603e26e77e4c3e51ff3d9d1f2
Jose Postiga <josepostiga@icloud.com>
2018-10-12 00:01:17 +0100

    Add RoleOrPermissionMiddleware

---
+++ src/Middlewares/RoleOrPermissionMiddleware.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Spatie\Permission\Middlewares;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+use Spatie\Permission\Exceptions\PermissionDoesNotExist;
+use Spatie\Permission\Exceptions\UnauthorizedException;
+
+class RoleOrPermissionMiddleware
+{
+    public function handle($request, Closure $next, $roleOrPermission)
+    {
+        if (Auth::guest()) {
+            throw UnauthorizedException::notLoggedIn();
+        }
+
+        $rolesOrPermissions = is_array($roleOrPermission)
+            ? $roleOrPermission
+            : explode('|', $roleOrPermission);
+
+        try {
+            if (! Auth::user()->hasAnyRole($rolesOrPermissions) || ! Auth::user()->hasAnyPermission($rolesOrPermissions)) {
+                throw UnauthorizedException::forRolesOrPermissions($rolesOrPermissions);
+            }
+        } catch (PermissionDoesNotExist $exception) {}
+
+        return $next($request);
+    }
+}
