8affad7bab1817403bf6cbbac6f14b76a3d9b040
Moeen <moeen.basra@gmail.com>
2019-06-30 08:16:21 +0400

    Add check on helper functions (#1105)
    Check if helper methods already exist, to avoid Lumen clash.

--- src/helpers.php
+++ src/helpers.php
@@ -1,23 +1,32 @@
 <?php
 
-/**
- * @param string $guard
- *
- * @return string|null
- */
-function getModelForGuard(string $guard)
-{
-    return collect(config('auth.guards'))
-        ->map(function ($guard) {
-            if (! isset($guard['provider'])) {
-                return;
-            }
+if (! function_exists('getModelForGuard')) {
+    /**
+     * @param string $guard
+     *
+     * @return string|null
+     */
+    function getModelForGuard(string $guard)
+    {
+        return collect(config('auth.guards'))
+            ->map(function ($guard) {
+                if (! isset($guard['provider'])) {
+                    return;
+                }
 
-            return config("auth.providers.{$guard['provider']}.model");
-        })->get($guard);
+                return config("auth.providers.{$guard['provider']}.model");
+            })->get($guard);
+    }
 }
 
-function isNotLumen() : bool
-{
-    return ! preg_match('/lumen/i', app()->version());
+if (! function_exists('isNotLumen')) {
+    /**
+     * check if application is lumen.
+     *
+     * @return bool
+     */
+    function isNotLumen(): bool
+    {
+        return ! preg_match('/lumen/i', app()->version());
+    }
 }
