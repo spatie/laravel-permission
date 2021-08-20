3cc2cbc62766d2d2e9c2f3e5be787f10d9ee74a2
Erik Niebla <ep_niebla@hotmail.com>
2021-08-20 09:17:54 -0500

    Avoid sync again other objects on Eloquent::saved

--- src/Traits/HasPermissions.php
+++ src/Traits/HasPermissions.php
@@ -348,6 +348,9 @@ trait HasPermissions
 
             $class::saved(
                 function ($object) use ($permissions, $model) {
+                    if ($model->getKey() != $object->getKey()) {
+                       return;
+                    }
                     $model->permissions()->sync($permissions, false);
                     $model->load('permissions');
                 }
