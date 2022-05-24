411eecaef445aa37ae40d58468671ff8e9c45b56
Sergei Zhidkov <zhidkoff@list.ru>
2022-05-24 08:41:19 +0400

    config() in Role and Permission models is deleted

--- src/Models/Permission.php
+++ src/Models/Permission.php
@@ -22,8 +22,6 @@ class Permission extends Model implements PermissionContract
 
     public function __construct(array $attributes = [])
     {
-        $attributes['guard_name'] = $attributes['guard_name'] ?? config('auth.defaults.guard');
-
         parent::__construct($attributes);
 
         $this->guarded[] = $this->primaryKey;
