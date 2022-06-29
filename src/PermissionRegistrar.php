f2303a70be60919811ca8afc313e8244fda00974
angeljqv <79208641+angeljqv@users.noreply.github.com>
2022-06-29 18:11:42 -0500

    Clear roles array after hydrate from cache (#2099)

--- src/PermissionRegistrar.php
+++ src/PermissionRegistrar.php
@@ -378,5 +378,7 @@ class PermissionRegistrar
             $role = $roleInstance->newFromBuilder($this->aliasedArray($item));
             $this->cachedRoles[$role->getKey()] = $role;
         }, $this->permissions['roles']);
+
+        $this->permissions['roles'] = [];
     }
 }
