e4493daedcab9aac91b9d1549ebcbad44f3f7326
erikn69 <erikn_69@hotmail.com>
2022-10-19 13:38:41 -0500

    Fix undefined index guard_name

--- src/Models/Permission.php
+++ src/Models/Permission.php
@@ -64,7 +64,7 @@ class Permission extends Model implements PermissionContract
     public function users(): BelongsToMany
     {
         return $this->morphedByMany(
-            getModelForGuard($this->attributes['guard_name']),
+            getModelForGuard($this->attributes['guard_name'] ?? config('auth.defaults.guard')),
             'model',
             config('permission.table_names.model_has_permissions'),
             PermissionRegistrar::$pivotPermission,
