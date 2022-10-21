6a4747d79b2118581a109d8082db09ebadbff5fe
Chris Brown <code@drbyte.dev>
2022-10-21 00:46:41 -0400

    Revert "Avoid calling the config helper in the role/perm model constructor"

--- src/Models/Role.php
+++ src/Models/Role.php
@@ -22,6 +22,8 @@ class Role extends Model implements RoleContract
 
     public function __construct(array $attributes = [])
     {
+        $attributes['guard_name'] = $attributes['guard_name'] ?? config('auth.defaults.guard');
+
         parent::__construct($attributes);
 
         $this->guarded[] = $this->primaryKey;
