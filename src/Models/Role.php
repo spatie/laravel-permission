1176b9f7a1121a7f18cdee7d2b07875c6e83ec59
Julio Motol <julio.motol89@gmail.com>
2022-10-26 23:40:54 +0800

    Revert "🐛 Override `getAllPermissions()` in `Role`"
    This reverts commit 4807005e6c299f0e0f8165c18a4c23d0d357d8d4.

--- src/Models/Role.php
+++ src/Models/Role.php
@@ -4,7 +4,6 @@ namespace Spatie\Permission\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Support\Collection;
 use Spatie\Permission\Contracts\Role as RoleContract;
 use Spatie\Permission\Exceptions\GuardDoesNotMatch;
 use Spatie\Permission\Exceptions\RoleAlreadyExists;
@@ -195,13 +194,5 @@ class Role extends Model implements RoleContract
         }
 
         return $this->permissions->contains($permission->getKeyName(), $permission->getKey());
-    }
-
-    /**
-     * Return all the permissions the model has, both directly and via roles.
-     */
-    public function getAllPermissions(): Collection
-    {
-        return $this->permissions->sort()->values();
     }
 }
