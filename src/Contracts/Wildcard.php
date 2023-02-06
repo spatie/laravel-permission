72e78cd07004e6caa5c8ffde5128bddf0c23a12a
drbyte <drbyte@users.noreply.github.com>
2023-02-06 17:08:47 +0000

    Fix styling

--- src/Contracts/Wildcard.php
+++ src/Contracts/Wildcard.php
@@ -5,8 +5,7 @@ namespace Spatie\Permission\Contracts;
 interface Wildcard
 {
     /**
-     * @param string|Wildcard $permission
-     *
+     * @param  string|Wildcard  $permission
      * @return bool
      */
     public function implies($permission): bool;
