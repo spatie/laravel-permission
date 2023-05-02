4e6b2eaab7f31dcc8690326c4c6bcd68b6b00439
erikn69 <erikn_69@hotmail.com>
2023-05-02 08:39:58 -0500

    Test touch on syncRoles, syncPermissions

--- tests/HasRolesWithCustomModelsTest.php
+++ tests/HasRolesWithCustomModelsTest.php
@@ -2,7 +2,9 @@
 
 namespace Spatie\Permission\Tests;
 
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
+use Spatie\Permission\Tests\TestModels\Admin;
 use Spatie\Permission\Tests\TestModels\Role;
 
 class HasRolesWithCustomModelsTest extends HasRolesTest
@@ -78,5 +80,24 @@ class HasRolesWithCustomModelsTest extends HasRolesTest
         $this->assertNull($role);
         $this->assertEquals(0, DB::table(config('permission.table_names.role_has_permissions'))->where('role_test_id', $role_id)->count());
         $this->assertEquals(0, DB::table(config('permission.table_names.model_has_roles'))->where('role_test_id', $role_id)->count());
+    }
+
+    /** @test */
+    public function it_should_touch_when_assigning_new_roles()
+    {
+        Carbon::setTestNow('2021-07-19 10:13:14');
+
+        $user = Admin::create(['email' => 'user1@test.com']);
+        $role1 = app(Role::class)->create(['name' => 'testRoleInWebGuard', 'guard_name' => 'admin']);
+        $role2 = app(Role::class)->create(['name' => 'testRoleInWebGuard1', 'guard_name' => 'admin']);
+
+        $this->assertSame('2021-07-19 10:13:14', $role1->updated_at->format('Y-m-d H:i:s'));
+
+        Carbon::setTestNow('2021-07-20 19:13:14');
+
+        $user->syncRoles([$role1->getKey(), $role2->getKey()]);
+
+        $this->assertSame('2021-07-20 19:13:14', $role1->refresh()->updated_at->format('Y-m-d H:i:s'));
+        $this->assertSame('2021-07-20 19:13:14', $role2->refresh()->updated_at->format('Y-m-d H:i:s'));
     }
 }
