9e7e4c23d0641e81f389ec35fd468a9ea0957e58
Chris Brown <code@drbyte.dev>
2023-08-21 11:31:31 -0400

    Add guard name to exceptions (#2481)
    * Add guard_name to DoesNotExist exceptions
    
    It's much easier to troubleshoot guard-related issues when the guard is included in the exception.

--- src/Models/Role.php
+++ src/Models/Role.php
@@ -103,7 +103,7 @@ class Role extends Model implements RoleContract
         $role = static::findByParam(['name' => $name, 'guard_name' => $guardName]);
 
         if (! $role) {
-            throw RoleDoesNotExist::named($name);
+            throw RoleDoesNotExist::named($name, $guardName);
         }
 
         return $role;
@@ -123,7 +123,7 @@ class Role extends Model implements RoleContract
         $role = static::findByParam([(new static())->getKeyName() => $id, 'guard_name' => $guardName]);
 
         if (! $role) {
-            throw RoleDoesNotExist::withId($id);
+            throw RoleDoesNotExist::withId($id, $guardName);
         }
 
         return $role;
