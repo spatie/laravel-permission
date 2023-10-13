6058b7c20f4aa156527c60a9b0153f1e6df660a1
Axel <axel_cg4@hotmail.com>
2023-10-13 14:00:27 -0600

    haspermission directive: same syntax as other directives

--- src/PermissionServiceProvider.php
+++ src/PermissionServiceProvider.php
@@ -123,15 +123,9 @@ class PermissionServiceProvider extends ServiceProvider
         $bladeCompiler->directive('elserole', fn ($args) => "<?php elseif({$bladeMethodWrapper}('hasRole', {$args})): ?>");
         $bladeCompiler->directive('endrole', fn () => '<?php endif; ?>');
 
-        $bladeCompiler->directive('haspermissionto', function ($arguments) {
-            return "<?php if(\\Spatie\\Permission\\PermissionServiceProvider::bladeMethodWrapper('hasPermissionTo', {$arguments})): ?>";
-        });
-        $bladeCompiler->directive('elsehaspermissionto', function ($arguments) {
-            return "<?php elseif(\\Spatie\\Permission\\PermissionServiceProvider::bladeMethodWrapper('hasPermissionTo', {$arguments})): ?>";
-        });
-        $bladeCompiler->directive('endhaspermissionto', function () {
-            return '<?php endif; ?>';
-        });
+        $bladeCompiler->directive('haspermissionto', fn ($arguments) => "<?php if(\\Spatie\\Permission\\PermissionServiceProvider::bladeMethodWrapper('hasPermissionTo', {$arguments})): ?>");
+        $bladeCompiler->directive('elsehaspermissionto', fn ($arguments) => "<?php elseif(\\Spatie\\Permission\\PermissionServiceProvider::bladeMethodWrapper('hasPermissionTo', {$arguments})): ?>");
+        $bladeCompiler->directive('endhaspermissionto', fn () => '<?php endif; ?>');
 
         $bladeCompiler->directive('hasrole', fn ($args) => "<?php if({$bladeMethodWrapper}('hasRole', {$args})): ?>");
         $bladeCompiler->directive('endhasrole', fn () => '<?php endif; ?>');
