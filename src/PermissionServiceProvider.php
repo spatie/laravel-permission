4e6e86d1a6a09f6b9ec6e92dd9cfcd6a614347b3
Axel Cervantes <axelscg@gmail.com>
2023-10-23 23:16:41 -0600

    Has permission directive - Non Default guard (#2515)
    * Has permission directive to use instead can directive when using non-default guard

--- src/PermissionServiceProvider.php
+++ src/PermissionServiceProvider.php
@@ -123,6 +123,10 @@ class PermissionServiceProvider extends ServiceProvider
         $bladeCompiler->directive('elserole', fn ($args) => "<?php elseif({$bladeMethodWrapper}('hasRole', {$args})): ?>");
         $bladeCompiler->directive('endrole', fn () => '<?php endif; ?>');
 
+        $bladeCompiler->directive('haspermission', fn ($args) => "<?php if({$bladeMethodWrapper}('checkPermissionTo', {$args})): ?>");
+        $bladeCompiler->directive('elsehaspermission', fn ($args) => "<?php elseif({$bladeMethodWrapper}('checkPermissionTo', {$args})): ?>");
+        $bladeCompiler->directive('endhaspermission', fn () => '<?php endif; ?>');
+
         $bladeCompiler->directive('hasrole', fn ($args) => "<?php if({$bladeMethodWrapper}('hasRole', {$args})): ?>");
         $bladeCompiler->directive('endhasrole', fn () => '<?php endif; ?>');
 
