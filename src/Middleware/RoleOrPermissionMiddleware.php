ed411f6608f1e9f9b20bee2d05e59c1c8c4354b7
SuperDJ <6484766+SuperDJ@users.noreply.github.com>
2023-11-15 15:37:59 +0000

    Hopefully fix issue for testing localy

--- src/Middleware/RoleOrPermissionMiddleware.php
+++ src/Middleware/RoleOrPermissionMiddleware.php
@@ -16,7 +16,7 @@ class RoleOrPermissionMiddleware
         $user = $authGuard->user();
 
         // For machine-to-machine Passport clients
-        if (! $user && $request->bearerToken() && config('permission.use_passport_client_credentials')) {
+        if (! $user && ($request->bearerToken() || auth()?->client()) && config('permission.use_passport_client_credentials')) {
             $user = Guard::getPassportClient($guard);
         }
 
