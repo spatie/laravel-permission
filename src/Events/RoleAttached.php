ac7dd81eb83f396c8f7d5040623e13caad47011d
Chris Brown <code@drbyte.dev>
2025-02-16 12:45:20 -0500

    Expand signature

--- src/Events/RoleAttached.php
+++ src/Events/RoleAttached.php
@@ -8,6 +8,8 @@ use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Collection;
+use Spatie\Permission\Contracts\Role;
 
 class RoleAttached
 {
@@ -15,6 +17,12 @@ class RoleAttached
     use InteractsWithSockets;
     use SerializesModels;
 
-    public function __construct(public Model $model, public array $roleIds)
-    {}
+    /**
+     * Internally the HasRoles trait passes an array of role ids (eg: int's or uuid's)
+     * Theoretically one could register the event to other places passing other types
+     * So a Listener should inspect the type of $rolesOrIds received before using.
+     *
+     * @param  array|int[]|string[]|Role|Role[]|Collection  $rolesOrIds
+     */
+    public function __construct(public Model $model, public mixed $rolesOrIds) {}
 }
