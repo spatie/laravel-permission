3b76bed0615277da3410cde52bbd9d6636c45a47
Chris Brown <code@drbyte.dev>
2025-02-16 12:35:23 -0500

    Expand incoming type

--- src/Events/PermissionAttached.php
+++ src/Events/PermissionAttached.php
@@ -8,6 +8,8 @@ use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Collection;
+use Spatie\Permission\Contracts\Permission;
 
 class PermissionAttached
 {
@@ -15,6 +17,12 @@ class PermissionAttached
     use InteractsWithSockets;
     use SerializesModels;
 
-    public function __construct(public Model $model, public array $permissionIds)
-    {}
+    /**
+     * Internally the HasPermissions trait passes an array of permission ids (eg: int's or uuid's)
+     * Theoretically one could register the event to other places and pass an Eloquent record.
+     * So a Listener should inspect the type of $permissionsOrIds received before using.
+     *
+     * @param  array|int[]|string[]|Permission|Permission[]|Collection  $permissionsOrIds
+     */
+    public function __construct(public Model $model, public mixed $permissionsOrIds) {}
 }
