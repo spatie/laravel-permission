c08e0f22d30707f398142382e43a451dc8125c1c
Chris Brown <code@drbyte.dev>
2025-02-16 12:40:26 -0500

    Expand event signature

--- src/Events/PermissionDetached.php
+++ src/Events/PermissionDetached.php
@@ -18,9 +18,11 @@ class PermissionDetached
     use SerializesModels;
 
     /**
-     * @param Model $model
-     * @param Permission|Permission[]|Collection $permission
+     * Internally the HasPermissions trait passes $permissionsOrIds as an Eloquent record.
+     * Theoretically one could register the event to other places and pass an array etc.
+     * So a Listener should inspect the type of $permissionsOrIds received before using.
+     *
+     * @param  array|int[]|string[]|Permission|Permission[]|Collection  $permissionsOrIds
      */
-    public function __construct(public Model $model, public mixed $permission)
-    {}
+    public function __construct(public Model $model, public mixed $permissionsOrIds) {}
 }
