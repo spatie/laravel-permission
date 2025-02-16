5afc854ca9c380363c0787cbc93ca6631f4eb01d
Chris Brown <code@drbyte.dev>
2025-02-16 12:45:50 -0500

    Expand signature

--- src/Events/RoleDetached.php
+++ src/Events/RoleDetached.php
@@ -8,6 +8,7 @@ use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Collection;
 use Spatie\Permission\Contracts\Role;
 
 class RoleDetached
@@ -16,6 +17,12 @@ class RoleDetached
     use InteractsWithSockets;
     use SerializesModels;
 
-    public function __construct(public Model $model, public Role $role)
-    {}
+    /**
+     * Internally the HasRoles trait passes $rolesOrIds as a single  Eloquent record
+     * Theoretically one could register the event to other places with an array etc
+     * So a Listener should inspect the type of $rolesOrIds received before using.
+     *
+     * @param  array|int[]|string[]|Role|Role[]|Collection  $rolesOrIds
+     */
+    public function __construct(public Model $model, public mixed $rolesOrIds) {}
 }
