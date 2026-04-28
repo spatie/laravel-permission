d91b3585f3b2a9413f4d26435e77a03f51f29d71
Freek Van der Herten <freek@spatie.be>
2026-04-28 13:12:38 +0200

    Add Support\Config class with typed accessors for team config

---
+++ src/Support/Config.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Spatie\Permission\Support;
+
+use Spatie\Permission\Exceptions\TeamModelNotConfigured;
+use Spatie\Permission\Exceptions\TeamsNotEnabled;
+use Spatie\Permission\PermissionRegistrar;
+
+class Config
+{
+    public static function teamsEnabled(): bool
+    {
+        return app(PermissionRegistrar::class)->teams;
+    }
+
+    public static function ensureTeamsEnabled(): void
+    {
+        if (! self::teamsEnabled()) {
+            throw TeamsNotEnabled::create();
+        }
+    }
+
+    /**
+     * @return class-string<\Illuminate\Database\Eloquent\Model>
+     */
+    public static function teamModel(): string
+    {
+        self::ensureTeamsEnabled();
+
+        $teamModel = app(PermissionRegistrar::class)->getTeamClass();
+
+        if (! $teamModel) {
+            throw TeamModelNotConfigured::create();
+        }
+
+        return $teamModel;
+    }
+
+    public static function modelHasRolesTable(): string
+    {
+        return config('permission.table_names.model_has_roles');
+    }
+
+    public static function morphKey(): string
+    {
+        return config('permission.column_names.model_morph_key');
+    }
+
+    public static function teamForeignKey(): string
+    {
+        return app(PermissionRegistrar::class)->teamsKey;
+    }
+}
