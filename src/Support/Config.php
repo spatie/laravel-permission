7ed3eeb75a61fc024cb7eeb79831499a4b9fe8f0
Freek Van der Herten <freek@spatie.be>
2026-04-28 14:19:33 +0200

    Expand Config with model, table, and feature-flag accessors

--- src/Support/Config.php
+++ src/Support/Config.php
@@ -3,9 +3,11 @@
 namespace Spatie\Permission\Support;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Permission\Contracts\Wildcard;
 use Spatie\Permission\Exceptions\TeamModelNotConfigured;
 use Spatie\Permission\Exceptions\TeamsNotEnabled;
 use Spatie\Permission\PermissionRegistrar;
+use Spatie\Permission\WildcardPermission;
 
 class Config
 {
@@ -42,6 +44,26 @@ class Config
         return config('permission.table_names.model_has_roles');
     }
 
+    public static function modelHasPermissionsTable(): string
+    {
+        return config('permission.table_names.model_has_permissions');
+    }
+
+    public static function roleHasPermissionsTable(): string
+    {
+        return config('permission.table_names.role_has_permissions');
+    }
+
+    public static function rolesTable(): string
+    {
+        return config('permission.table_names.roles');
+    }
+
+    public static function permissionsTable(): string
+    {
+        return config('permission.table_names.permissions');
+    }
+
     public static function morphKey(): string
     {
         return config('permission.column_names.model_morph_key');
@@ -50,5 +72,54 @@ class Config
     public static function teamForeignKey(): string
     {
         return app(PermissionRegistrar::class)->teamsKey;
+    }
+
+    /**
+     * @return class-string<Model>
+     */
+    public static function roleModel(): string
+    {
+        return app(PermissionRegistrar::class)->getRoleClass();
+    }
+
+    /**
+     * @return class-string<Model>
+     */
+    public static function permissionModel(): string
+    {
+        return app(PermissionRegistrar::class)->getPermissionClass();
+    }
+
+    public static function eventsEnabled(): bool
+    {
+        return (bool) config('permission.events_enabled');
+    }
+
+    public static function usePassportClientCredentials(): bool
+    {
+        return (bool) config('permission.use_passport_client_credentials');
+    }
+
+    public static function displayRoleInException(): bool
+    {
+        return (bool) config('permission.display_role_in_exception');
+    }
+
+    public static function displayPermissionInException(): bool
+    {
+        return (bool) config('permission.display_permission_in_exception');
+    }
+
+    public static function wildcardPermissionsEnabled(): bool
+    {
+        return (bool) config('permission.enable_wildcard_permission');
+    }
+
+    /**
+     * @return class-string<Wildcard>
+     */
+    public static function wildcardPermissionClass(): string
+    {
+        return config('permission.wildcard_permission', WildcardPermission::class);
     }
 }
