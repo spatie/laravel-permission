a304587fce37dda88c8e748a324d1a17cb8c6d54
Majid Alaeinia <11965368+majidalaeinia@users.noreply.github.com>
2026-06-17 13:19:04 +0330

    Resolve grammar issue in comments

--- config/permission.php
+++ config/permission.php
@@ -11,7 +11,7 @@ return [
         /*
          * When using the "HasPermissions" trait from this package, we need to know which
          * Eloquent model should be used to retrieve your permissions. Of course, it
-         * is often just the "Permission" model but you may use whatever you like.
+         * is often just the "Permission" model, but you may use whatever you like.
          *
          * The model you want to use as a Permission model needs to implement the
          * `Spatie\Permission\Contracts\Permission` contract.
@@ -22,7 +22,7 @@ return [
         /*
          * When using the "HasRoles" trait from this package, we need to know which
          * Eloquent model should be used to retrieve your roles. Of course, it
-         * is often just the "Role" model but you may use whatever you like.
+         * is often just the "Role" model, but you may use whatever you like.
          *
          * The model you want to use as a Role model needs to implement the
          * `Spatie\Permission\Contracts\Role` contract.
